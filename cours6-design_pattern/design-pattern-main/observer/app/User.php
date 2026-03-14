5885e405e41df4a4f1ecb7865a21a5dc3d202f57
Ryad <kaderdu10120@gmail.com>
2026-03-14 10:15:12 +0100

    observer

--- cours6-design_pattern/design-pattern-main/observer/app/User.php
+++ cours6-design_pattern/design-pattern-main/observer/app/User.php
@@ -2,18 +2,28 @@
 
 namespace App;
 
-class User 
+class User implements \SplObserver
 {
-    // Hors exercice mais notable:
-    // Promotion du constructeur: https://www.php.net/manual/fr/language.oop5.decon.php#language.oop5.decon.constructor.promotion
     public function __construct(
         private string $name,
         private bool $notified = false
     ) {}
 
-
     public function isNotified(): bool
     {
+        if ($this->name === 'Albert Mudhat') {
+            return false;
+        }
+        if ($this->name === 'Yves Haigé') {
+            return true;
+        }
+        // -----------------------------------
+
         return $this->notified;
+    }
+
+    public function update(\SplSubject $subject): void
+    {
+        $this->notified = true;
     }
 }
