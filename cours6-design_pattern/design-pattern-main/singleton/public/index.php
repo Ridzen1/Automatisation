2ba8e4554a6908287066f66247c71ef54bb38d13
Ryad <kaderdu10120@gmail.com>
2026-03-14 11:03:07 +0100

    singleton

--- cours6-design_pattern/design-pattern-main/singleton/public/index.php
+++ cours6-design_pattern/design-pattern-main/singleton/public/index.php
@@ -1,7 +1,24 @@
 <?php
 require('../vendor/autoload.php');
 
-die('ici vous pouvez tester votre singleton !');
-# TODO: Récuperer une instance de Config
-# Afficher une valeur contenu dans config.php
-# Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+use App\Config;
+
+echo "<h1>Test du pattern Singleton</h1>";
+
+$config1 = Config::getInstance();
+
+echo "<p><strong>Valeur de la clé 'apiKey' :</strong> " . $config1->get('apiKey') . "</p>";
+echo "<p><strong>Valeur de la clé 'db' -> 'host' :</strong> " . $config1->get('db')['host'] . "</p>";
+
+$config2 = Config::getInstance();
+
+echo "<p><strong>Les deux instances sont-elles identiques ?</strong> ";
+if ($config1 === $config2) {
+    echo "Oui ! Le Singleton fonctionne. (bool: true)</p>";
+} else {
+    echo "Non... Il y a un problème. (bool: false)</p>";
+}
+
+echo "<pre>";
+var_dump($config1, $config2);
+echo "</pre>";
